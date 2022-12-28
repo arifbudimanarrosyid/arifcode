85b1476f1ec43d24fa711a8970c70ca61519fae6
arifbudimanar <arifbudiman01041996@hotmail.com>
2022-12-29 03:46:15 +0700

    fix profile form padding

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,28 +7,28 @@
 
     <div class="py-12">
         <div class="px-4 mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
-            <div class="p-4 bg-white rounded-lg sm:p-8 dark:bg-gray-800">
-                <div class="max-w-xl">
+            <div class="p-4 bg-white rounded-lg dark:bg-gray-800">
+                <div class="max-w-3xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
             @if (Auth::user()->password)
-            <div class="p-4 bg-white rounded-lg sm:p-8 dark:bg-gray-800">
-                <div class="max-w-xl">
+            <div class="p-4 bg-white rounded-lg dark:bg-gray-800">
+                <div class="max-w-3xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 bg-white rounded-lg sm:p-8 dark:bg-gray-800">
-                <div class="max-w-xl">
+            <div class="p-4 bg-white rounded-lg dark:bg-gray-800">
+                <div class="max-w-3xl">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
             @endif
             @if (Auth::user()->password == null)
-            <div class="p-4 bg-white rounded-lg sm:p-8 dark:bg-gray-800">
-                <div class="max-w-xl">
+            <div class="p-4 bg-white rounded-lg dark:bg-gray-800">
+                <div class="max-w-3xl">
                     @include('profile.partials.delete-user-with-no-password')
                 </div>
             </div>
