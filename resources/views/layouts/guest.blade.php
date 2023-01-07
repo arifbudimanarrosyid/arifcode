5ed899ab0f72e084122dea5c66bf37bded10426e
arifbudimanar <arifbudiman01041996@hotmail.com>
2023-01-07 10:53:42 +0700

    add home banner on md screen

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -41,6 +41,16 @@
 </head>
 
 <body class="font-sans antialiased bg-gray-100 dark:bg-gray-900">
+    @if (Route::currentRouteName() == 'home')
+    <div class="hidden text-gray-500 bg-gray-200 dark:bg-gray-800 dark:text-gray-300 md:block">
+        <div class="flex flex-col items-center justify-center max-w-3xl p-3 mx-auto text-sm" role="alert">
+            <p class="text-center">Developed with Laravel 9, Breeze, Tailwind CSS, AlpineJS, HyperJS, UI from Flowbite
+                and Hyper UI.</p>
+            <p>You guys can check out on <a href="https://github.com/arifbudimanarrosyid/arifcode"
+                    class="text-indigo-500">Github</a>.</p>
+        </div>
+    </div>
+    @endif
     <div class="max-w-3xl min-h-screen mx-auto">
         @include('layouts.guest-navigation')
         {{-- <div class="sm:px-6 lg:px-8">
