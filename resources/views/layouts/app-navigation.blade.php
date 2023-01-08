ec48910aa943629d9e51c42957d79bf5f7e2bf75
arifbudimanar <arifbudiman01041996@hotmail.com>
2023-01-08 13:07:32 +0700

    change dark mode bg

--- resources/views/layouts/app-navigation.blade.php
+++ resources/views/layouts/app-navigation.blade.php
@@ -40,9 +40,57 @@
             </div>
 
             <div class="flex">
+                {{-- Dark Mode Dropdown --}}
+                <div class="hidden sm:flex sm:items-center sm:ml-2">
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button
+                                class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-400 transition duration-150 ease-in-out border border-transparent rounded-md hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
+                                <div>
+                                    {{-- Dark Mode --}}
+                                    <svg id="theme-toggle-dark-icon" class="w-5 h-5" fill="currentColor"
+                                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z">
+                                        </path>
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
 
+                        <x-slot name="content">
+                            <button x-on:click="darkMode = 'dark', showDropdown = false"
+                                class="flex w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 transition duration-150 ease-in-out dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800">
+                                <svg id="theme-toggle-dark-icon" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                                </svg>
+                                <span class="ml-2">Dark</span>
+                            </button>
+                            <button x-on:click="darkMode = 'light', showDropdown = false"
+                                class="flex w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 transition duration-150 ease-in-out dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800">
+                                <svg id="theme-toggle-light-icon" class="w-5 h-5" fill="currentColor"
+                                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                        fill-rule="evenodd" clip-rule="evenodd"></path>
+                                </svg>
+                                <span class="ml-2">Light</span>
+                            </button>
+                            <button x-on:click="darkMode = 'system', showDropdown = false"
+                                class="flex w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 transition duration-150 ease-in-out dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                    class="w-5 h-5">
+                                    <path fill-rule="evenodd"
+                                        d="M2.25 5.25a3 3 0 013-3h13.5a3 3 0 013 3V15a3 3 0 01-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 01-.53 1.28h-9a.75.75 0 01-.53-1.28l.621-.622a2.25 2.25 0 00.659-1.59V18h-3a3 3 0 01-3-3V5.25zm1.5 0v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                                <span class="ml-2">System</span>
+                            </button>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
                 <!-- User Dropdown -->
-                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <div class="hidden sm:flex sm:items-center sm:ml-2">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button
@@ -80,55 +128,7 @@
                         </x-slot>
                     </x-dropdown>
                 </div>
-                {{-- Dark Mode Dropdown --}}
-                <div class="hidden sm:flex sm:items-center sm:ml-2">
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            <button
-                                class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-400 transition duration-150 ease-in-out border border-transparent rounded-md hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
-                                <div>
-                                    {{-- Dark Mode --}}
-                                    <svg id="theme-toggle-dark-icon" class="w-5 h-5" fill="currentColor"
-                                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z">
-                                        </path>
-                                    </svg>
-                                </div>
-                            </button>
-                        </x-slot>
 
-                        <x-slot name="content">
-                            <button x-on:click="darkMode = 'dark', showDropdown = false"
-                                class="flex w-full px-3 py-2 text-sm text-gray-500 bg-white dark:bg-gray-800 dark:text-gray-400 focus:outline-none">
-                                <svg id="theme-toggle-dark-icon" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                                </svg>
-                                <span class="ml-2">Dark</span>
-                            </button>
-                            <button x-on:click="darkMode = 'light', showDropdown = false"
-                                class="flex w-full px-3 py-2 text-sm text-gray-500 bg-white dark:bg-gray-800 dark:text-gray-400 focus:outline-none">
-                                <svg id="theme-toggle-light-icon" class="w-5 h-5" fill="currentColor"
-                                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                                        fill-rule="evenodd" clip-rule="evenodd"></path>
-                                </svg>
-                                <span class="ml-2">Light</span>
-                            </button>
-                            <button x-on:click="darkMode = 'system', showDropdown = false"
-                                class="flex w-full px-3 py-2 text-sm text-gray-500 bg-white dark:bg-gray-800 dark:text-gray-400 focus:outline-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                    class="w-5 h-5">
-                                    <path fill-rule="evenodd"
-                                        d="M2.25 5.25a3 3 0 013-3h13.5a3 3 0 013 3V15a3 3 0 01-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 01-.53 1.28h-9a.75.75 0 01-.53-1.28l.621-.622a2.25 2.25 0 00.659-1.59V18h-3a3 3 0 01-3-3V5.25zm1.5 0v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                                <span class="ml-2">System</span>
-                            </button>
-                        </x-slot>
-                    </x-dropdown>
-                </div>
             </div>
 
 
@@ -173,7 +173,7 @@
             </div>
             <div class="pt-4 pb-4 border-t border-gray-200 dark:border-gray-600">
                 <button x-on:click="darkMode = 'dark', showDropdown = false"
-                    class="flex w-full px-3 py-2 text-sm text-gray-500 bg-white dark:bg-gray-800 dark:text-gray-400 focus:outline-none">
+                    class="flex w-full py-2 pl-3 pr-4 text-base font-medium text-left text-gray-600 transition duration-150 ease-in-out border-l-4 border-transparent dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600">
                     <svg id="theme-toggle-dark-icon" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg">
                         <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
@@ -181,7 +181,7 @@
                     <span class="ml-2">Dark</span>
                 </button>
                 <button x-on:click="darkMode = 'light', showDropdown = false"
-                    class="flex w-full px-3 py-2 text-sm text-gray-500 bg-white dark:bg-gray-800 dark:text-gray-400 focus:outline-none">
+                    class="flex w-full py-2 pl-3 pr-4 text-base font-medium text-left text-gray-600 transition duration-150 ease-in-out border-l-4 border-transparent dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600">
                     <svg id="theme-toggle-light-icon" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg">
                         <path
@@ -191,7 +191,7 @@
                     <span class="ml-2">Light</span>
                 </button>
                 <button x-on:click="darkMode = 'system', showDropdown = false"
-                    class="flex w-full px-3 py-2 text-sm text-gray-500 bg-white dark:bg-gray-800 dark:text-gray-400 focus:outline-none">
+                    class="flex w-full py-2 pl-3 pr-4 text-base font-medium text-left text-gray-600 transition duration-150 ease-in-out border-l-4 border-transparent dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
                         <path fill-rule="evenodd"
                             d="M2.25 5.25a3 3 0 013-3h13.5a3 3 0 013 3V15a3 3 0 01-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 01-.53 1.28h-9a.75.75 0 01-.53-1.28l.621-.622a2.25 2.25 0 00.659-1.59V18h-3a3 3 0 01-3-3V5.25zm1.5 0v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5z"
@@ -213,7 +213,8 @@
                     <x-responsive-nav-link :href="route('home')">
                         {{ __('Home') }}
                         </x-response-nav-link>
-                        <x-responsive-nav-link :href="route('profile.edit')">
+                        <x-responsive-nav-link :href="route('profile.edit')"
+                            :active="request()->routeIs('profile.edit')">
                             {{ __('Profile') }}
                             </x-response-nav-link>
 
@@ -236,8 +237,8 @@
                             class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-400 transition duration-150 ease-in-out border border-transparent rounded-md hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
                             <div>
                                 {{-- Dark Mode --}}
-                                <svg id="theme-toggle-dark-icon" class="w-5 h-5" fill="currentColor"
-                                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <svg id="theme-toggle-dark-icon" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
                                     <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z">
                                     </path>
                                 </svg>
@@ -256,8 +257,8 @@
                         </button>
                         <button x-on:click="darkMode = 'light', showDropdown = false"
                             class="flex w-full px-3 py-2 text-sm text-gray-500 bg-white dark:bg-gray-800 dark:text-gray-400 focus:outline-none">
-                            <svg id="theme-toggle-light-icon" class="w-5 h-5" fill="currentColor"
-                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <svg id="theme-toggle-light-icon" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
                                 <path
                                     d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
                                     fill-rule="evenodd" clip-rule="evenodd"></path>
