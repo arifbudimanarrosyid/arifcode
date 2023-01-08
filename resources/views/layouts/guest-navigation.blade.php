f0498aa9e1f57669bfb4e3290d89e206712bf73b
arifbudimanar <arifbudiman01041996@hotmail.com>
2023-01-08 12:42:35 +0700

    fix register button

--- resources/views/layouts/guest-navigation.blade.php
+++ resources/views/layouts/guest-navigation.blade.php
@@ -51,12 +51,12 @@
                         Login
                     </a>
                     @endif
-                    {{-- @if (Route::has('register'))
+                    @if (Route::has('register'))
                     <a href="{{ route('register') }}"
-                        class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-400 transition duration-150 ease-in-out bg-white border border-transparent rounded-md dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
+                        class="items-center hidden px-3 py-2 text-sm font-medium leading-4 text-gray-400 transition duration-150 ease-in-out bg-white border border-transparent rounded-md md:block dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none">
                         Register
                     </a>
-                    @endif --}}
+                    @endif
 
 
                 </div>
